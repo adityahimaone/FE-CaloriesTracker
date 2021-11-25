41b1462aaacf2cacb4ee167ef25d8a0b6e228425
Aditya Himawan <adityahimaone@gmail.com>
2021-11-25 17:21:50 +0700

    fix get history

--- src/containers/dashboard/components/TabsGroup.jsx
+++ src/containers/dashboard/components/TabsGroup.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Tab } from "@headlessui/react";
 import CardFood from "./CardFood";
+import Lottie from "react-lottie";
+import animationData from "../../../assets/img/loading.json";
 
 export default function TabsGroup(props) {
-  console.log("dari tabs grup", props);
-  if (props.loading) {
-    return <div>Loading...</div>;
-  }
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <Tab.Group>
       <Tab.List className=" w-full tabs">
@@ -28,15 +34,19 @@ export default function TabsGroup(props) {
       </Tab.List>
       <Tab.Panels>
         <Tab.Panel className="bg-white max-w-full p-2 sm:p-2 border border-gray-300 border-t-0  rounded-b-lg border-opacity-100">
-          {props.historyData?.calories_tracker_histories.map((item) => {
+          {props.historyData?.calories_tracker_histories.map((item) => (
             <CardFood
               key={item.id}
-              data={item.food}
-              // calorie={item.food.calorie}
-            />;
-            // console.log(item.food.foodUrl)
-            // console.log("item",item)
-          })}
+              title={item.food.name}
+              image={item.food.foodUrl}
+              calorie={item.food.calorie}
+            />
+          ))}
+          {props.loading ? (
+            <div className="mx-auto">
+              <Lottie options={defaultOptions} height={300} width={500} />
+            </div>
+          ) : null}
           {/* <CardFood
             image="http://inibaru.id/media/5100/large/normal/b879e94c-9dfd-4b9d-afa6-4759066cdc60__large.jpg"
             title="Nasi Padang"
